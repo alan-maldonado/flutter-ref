91e09c64701c3b2bd08c819753543f62d9d79c36
alan-maldonado <alan.maldonado@wizeline.com>
2018-09-02 10:23:15 -0500

    ✨ Repository pattern refactor

--- news/lib/src/resources/news_db_provider.dart
+++ news/lib/src/resources/news_db_provider.dart
@@ -4,9 +4,18 @@ import 'dart:io';
 import 'package:path/path.dart';
 import 'dart:async';
 import '../models/item_model.dart';
+import 'repository.dart';
 
-class NewsDbProvider {
+class NewsDbProvider implements Source, Cache {
   Database db;
+
+  Future<List<int>> fetchTopIds() {
+    return null;
+  }
+
+  NewsDbProvider() {
+    init();
+  }
 
   void init() async {
     Directory documentsDirectory = await getApplicationDocumentsDirectory();
@@ -56,3 +65,5 @@ class NewsDbProvider {
     return db.insert('Items', item.toMap());
   }
 }
+
+final newsDbProvider = NewsDbProvider();
