173352e00084a4b2e2d304abccb140d0a8332f56
alan-maldonado <alan.maldonado@wizeline.com>
2018-09-02 14:35:26 -0500

    💄 Rendering top Ids in a list

--- news/lib/src/screens/news_list.dart
+++ news/lib/src/screens/news_list.dart
@@ -4,12 +4,31 @@ import '../blocs/stories_provider.dart';
 class NewsList extends StatelessWidget {
   Widget build(context) {
     final bloc = StoriesProvider.of(context);
+    bloc.fetchTopIds(); // JUST FOR TESTING
 
     return Scaffold(
       appBar: AppBar(
         title: Text('Top News'),
       ),
-      body: Text('Show a list'),
+      body: buildList(bloc),
+    );
+  }
+
+  Widget buildList(StoriesBloc bloc) {
+    return StreamBuilder(
+      stream: bloc.topIds,
+      builder: (context, AsyncSnapshot<List<int>> snapshot) {
+        if (!snapshot.hasData) {
+          return Text('Still waiting on Ids');
+        }
+
+        return ListView.builder(
+          itemCount: snapshot.data.length,
+          itemBuilder: (context, int index) {
+            return Text('${snapshot.data[index]}');
+          },
+        );
+      },
     );
   }
 }
