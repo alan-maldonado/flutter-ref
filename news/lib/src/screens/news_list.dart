0d3e27ce8f38bf2bb006813fced6a9a212e1e19b
alan-maldonado <alan.maldonado@wizeline.com>
2018-09-02 13:16:19 -0500

    ⚡️ Rendering list view on demand

--- news/lib/src/screens/news_list.dart
+++ news/lib/src/screens/news_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 class NewsList extends StatelessWidget {
   Widget build(context) {
@@ -6,7 +7,29 @@ class NewsList extends StatelessWidget {
       appBar: AppBar(
         title: Text('Top News'),
       ),
-      body: Text('Show top news'),
+      body: buildList(),
     );
+  }
+
+  Widget buildList() {
+    return ListView.builder(
+      itemCount: 1000,
+      itemBuilder: (context, int index) {
+        return FutureBuilder(
+            future: getFuture(),
+            builder: (context, snapshot) {
+              return Container(
+                height: 80.0,
+                child: snapshot.hasData
+                    ? Text('Im visible $index')
+                    : Text('I havent fetched data $index'),
+              );
+            });
+      },
+    );
+  }
+
+  Future<String> getFuture() {
+    return Future.delayed(Duration(seconds: 2), () => 'hi');
   }
 }
