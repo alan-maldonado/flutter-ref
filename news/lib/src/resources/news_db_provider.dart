28005b5b4e9ae8c44d7afa8ee711ec828d64f40b
alan-maldonado <alan.maldonado@wizeline.com>
2018-09-03 01:49:28 -0500

    ♻️ Refresh results

--- news/lib/src/resources/news_db_provider.dart
+++ news/lib/src/resources/news_db_provider.dart
@@ -67,6 +67,10 @@ class NewsDbProvider implements Source, Cache {
       item.toMap(),
     );
   }
+
+  Future<int> clear() {
+    return db.delete('Items');
+  }
 }
 
 final newsDbProvider = NewsDbProvider();
