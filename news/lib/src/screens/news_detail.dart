b55fe4960af5becc1dfa7557616fa15c78fc713f
alan-maldonado <alan.maldonado@wizeline.com>
2018-09-05 16:08:34 -0500

    ✨ Communicate itemId to second screen

--- news/lib/src/screens/news_detail.dart
+++ news/lib/src/screens/news_detail.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 
 class NewsDetail extends StatelessWidget {
+  final int itemId;
+  NewsDetail({this.itemId});
+
   Widget build(context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Detail'),
       ),
-      body: Text('I am a news detail'),
+      body: Text('$itemId'),
     );
   }
 }
