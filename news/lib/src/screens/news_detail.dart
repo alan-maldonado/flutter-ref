f1330094a232cfc19b38478786386c69cf110e12
alan-maldonado <alan.maldonado@wizeline.com>
2018-09-05 17:45:01 -0500

    🏗 Resolving future and reading streambuilder

--- news/lib/src/screens/news_detail.dart
+++ news/lib/src/screens/news_detail.dart
@@ -1,15 +1,44 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import '../models/item_model.dart';
+import '../blocs/comments_provider.dart';
 
 class NewsDetail extends StatelessWidget {
   final int itemId;
   NewsDetail({this.itemId});
 
   Widget build(context) {
+    final bloc = CommentsProvider.of(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Detail'),
       ),
-      body: Text('$itemId'),
+      body: buildBody(bloc),
+    );
+  }
+
+  Widget buildBody(CommentsBloc bloc) {
+    return StreamBuilder(
+      stream: bloc.itemWithComments,
+      builder: (context, AsyncSnapshot<Map<int, Future<ItemModel>>> snapshot) {
+        if (!snapshot.hasData) {
+          return Text('Loading');
+        }
+
+        final itemFuture = snapshot.data[itemId];
+
+        return FutureBuilder(
+          future: itemFuture,
+          builder: (context, AsyncSnapshot<ItemModel> itemSnapshot) {
+            if (!snapshot.hasData) {
+              return Text('Loading');
+            }
+
+            return Text(itemSnapshot.data.title);
+          },
+        );
+      },
     );
   }
 }
